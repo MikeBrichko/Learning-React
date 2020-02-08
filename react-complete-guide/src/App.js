9c522ac83562cd0b73d71a421bfd96f206e2465d
w.mike.brichko@gmail.com <w.mike.brichko@gmail.com>
2020-02-08 10:34:15 -0500

    Understanding react basic features and syntax

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+  state = {
+    persons: [
+      {name: "Jeff", age: "42"},
+      {name: "Lacile", age: "73"},
+      {name: "Todd", age: "13"}
+    ]
+  };
+
+  clickHandler = (name) => {
+    console.log("was clicked");
+    this.setState({
+      persons: [
+        {name: name, age: "42"},
+        {name: "Lacile", age: "73"},
+        {name: "Todd", age: "13"}
+      ],
+      troll: "I am a troll!"
+    });
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        {name: event.target.value, age: "42"},
+        {name: "Lacile", age: "73"},
+        {name: "Todd", age: "13"}
+      ],
+      troll: "I am a troll!"
+    });
+  }
+
+  render () {
+    return (
+      <div className="App">
+      <h1>I am a react header hehehe</h1>
+      <button onClick={this.clickHandler.bind(this, "Maxwell")}>Press this button</button>
+      <Person
+        changed={this.nameChangedHandler}
+        name={this.state.persons[0].name} 
+        age={this.state.persons[0].age}/>
+      <Person 
+        name={this.state.persons[1].name} 
+        age={this.state.persons[1].age}
+        click={() => this.clickHandler("Martin")}>{this.state.troll}</Person>
+      <Person 
+        name={this.state.persons[2].name} 
+        age={this.state.persons[2].age}/>
     </div>
-  );
+    )
+  };
 }
 
 export default App;
---
+++ react-complete-guide/src/Person/Person.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Card from "@material-ui/core/Card"
+import './Person.css'
+
+const person = (props) => {
+return (
+    <Card className="card">
+        <h1 onClick={props.click}>My name is {props.name} and I am {props.age} years old</h1>
+        <p>{props.children}</p>
+        <input type="text" onChange={props.changed} value={props.name}></input>
+    </Card>
+    )
+}
+
+export default person;
